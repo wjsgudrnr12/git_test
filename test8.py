00ba947aaa6b2101bcb40ddf33223d9166f926d7
wjsgudrnr12 <hkjun@etri.re.kr>
2026-06-29 20:35:43 +0900

    test8 edit, test9 create

--- test8.py
+++ test8.py
@@ -1,12 +1,1 @@
-<<<<<<< HEAD
-8-18181818181
-=======
-8888888888888888888888888
-<<<<<<< HEAD
->>>>>>> su
-pridddsssssssssssssb
-=======
->>>>>>> sub
-
-print(test)
->>>>>>> e0de794ec9aefd9279c44413e5e2835534df229e
+test8.old
