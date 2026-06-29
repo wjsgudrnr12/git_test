faac78665c1f14e0ea5fd09d33ed2d04918f9826
wjsgudrnr12 <hkjun@etri.re.kr>
2026-06-29 20:36:53 +0900

    test8.py edit

--- test8.py
+++ test8.py
@@ -1,1 +1,19 @@
+<<<<<<< HEAD
 test8.old
+=======
+<<<<<<< HEAD
+8-18181818181
+=======
+8888888888888888888888888
+<<<<<<< HEAD
+>>>>>>> su
+pridddsssssssssssssb
+=======
+>>>>>>> sub
+
+print(test)
+>>>>>>> e0de794ec9aefd9279c44413e5e2835534df229e
+
+
+junjunjun
+>>>>>>> c1091c5 (test8.py edit)
